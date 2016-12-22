54ab4d855f1be292f5813ed8e139043e71317c76
Veesibility <Veesibility-test@users.noreply.github.com>
2016-12-22 12:05:36 +0100

    Update customplugin.php

--- customplugin.php
+++ customplugin.php
@@ -1,7 +1,7 @@
 <?php 
     /*
     Plugin Name: Custom Plugin
-    Plugin URI: https://github.com/Veesibility-test/customplugin.git
+    Github Plugin URI: https://github.com/Veesibility-test/customplugin.git
     Description: Plugin for displaying products from an OSCommerce shopping cart database
     Author: Veesibility
     Version: 1.0
