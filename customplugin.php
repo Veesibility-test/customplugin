2b9226aa0d2905a3ba4b1b896f586aea29b00d39
Veesibility <Veesibility-test@users.noreply.github.com>
2016-12-27 13:13:04 +0100

    Update customplugin.php

--- customplugin.php
+++ customplugin.php
@@ -1,7 +1,7 @@
 <?php 
 /**
  * Plugin Name:      Custom Plugin
- * Version:          5
+ * Version:          6
  * Description:      Custom Plugin
  * Author:           Veesibility
  * GitHub Plugin URI: https://github.com/Veesibility-test/customplugin
